6c95d3120a004d78cea7b7e8a56c3564d95fdc2b
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-11-10 11:20:12 -0300

    Criação do projeto no Xcode do aplicativo "vamos pra onde?"

---
+++ vamosPraOndeApp/vamosPraOndeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vamosPraOndeApp
+//
+//  Created by Ronaldo Ribeiro on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
