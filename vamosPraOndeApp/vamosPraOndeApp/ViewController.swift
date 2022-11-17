931af28a12f94091cc5d9784c4a51b9a1bb639ea
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-11-17 19:07:27 -0300

    Adição de elementos na tela de login

--- vamosPraOndeApp/vamosPraOndeApp/ViewController.swift
+++ vamosPraOndeApp/vamosPraOndeApp/ViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginLabel: UILabel!
+    
+    @IBOutlet weak var instructionsLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextLabel: UITextField!
+    
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
